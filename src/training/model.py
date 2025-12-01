d99d6e61a497fdc342e15cd534e450372f1b32df
rick <rickhonda0@gmail.com>
2025-12-01 20:48:36 +1100

    feat: add custom neural net class for predicting returns\
    No special features planned yet, thinking abt including:
    - momentum indicators
    - liquidity indicators
    - volatility indicators
    
    Then, probably good to orthoganalise features to avoid correlated
    features and reduce multicollinearity

---
+++ src/training/model.py
@@ -0,0 +1,24 @@
+import torch.nn as nn
+
+class MLPModel(nn.module):
+    def __init__(self, d_features, n_assets, *h_layers):
+        # (i, h_layers[i]) = i'th hidden layer and the number of neurons in that layer 
+        assert all(isinstance(x, int) for x in h_layers)
+        super().__init__()
+
+        # build list of linear layers based on h_layers  
+        layers = []
+        input_layer = d_features
+        
+        for hidden_layer in h_layers:
+            layers.append(nn.Linear(in_features=input_layer, out_features=hidden_layer))
+            # for now, just ReLU. Consider LSTM later.
+            layers.append(nn.ReLU())
+            input_layer = hidden_layer
+
+        layers.append(nn.Linear(in_features=input_layer, out_features=n_assets))
+
+        self.net = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.net
