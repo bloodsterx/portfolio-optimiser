2efd36662cbf7c653804cd1d7dbf04448a888f47
rick <rickhonda0@gmail.com>
2025-12-02 13:00:27 +1100

    feat: add super basic training loop with limited diagnostics. Will add more in-depth training details

--- src/training/trainer.py
+++ src/training/trainer.py
@@ -33,6 +33,41 @@ def train(
             # TODO
             pass
 
+    output = []
+
+    for epoch in range(n_epochs):
+        # in train mode
+
+        model.train()
+        cum_loss = 0.0
+
+        for X_batch, C_batch in dataloader:
+            # move data to device? (what does this do)
+            X_batch = X_batch.to(device)
+            C_batch = C_batch.to(device)
+
+            # 1. forward pass
+            c_hat = model(X_batch)
+
+            # 2. calc (short for calculator btw (short for by the way)) loss
+            loss = loss_fn(c_hat, C_batch)
+
+            # 3. clear old gradients
+            optimizer.zero_grad()
+
+            # 4. backprop
+            loss.backwards() # traverse backprop graph and recomputes gradients for each node
+            optimizer.step() # applies the gradient updates
+
+            cum_loss += loss.item() * X_batch.size(0)
+
+            output.append(
+                f"Epoch [{epoch}]: cumulative loss={cum_loss}"
+            )
+    
+    return model
+
+
    
         
 
