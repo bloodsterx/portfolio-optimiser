47abf77380b2e67c914f38a2e314a83bdb0cef09
rick <rickhonda0@gmail.com>
2025-12-03 12:19:17 +1100

    refactor: combine multiple momentum functions into one which accepts 'window' parameter. Can essentially do everything the old functions could do by setting value for window
    if window = 12, then window_size=11 (in polars, calculated on the
    current + previous 11 rows)

--- src/data/features.py
+++ src/data/features.py
@@ -33,6 +33,36 @@ class Features:
         self.date_col = date_col
         self.asset_cols = [col for col in data.columns if col != date_col]
         self.n_assets = len(self.asset_cols)
+
+    def mom(self, window, units="m") -> pl.DataFrame:
+        """
+        Returns a matrix of lagged-1m stock returns
+
+        Args:
+            window: int - interval over which to calculate momentum. Units in terms of input data.
+            E.g. window=12 => compounded return over 12 rows. If rows correspond to months, then window=12 => 12 month momentum
+
+            units: (d, w, m, y)
+        """
+        match units.lower():
+            case "d" | "w" | "m" | "y":
+                pass
+            case _:
+                print(f"{'='*10} Exception Here {'='*10}")
+
+        result = self.data.select(
+            pl.col(self.date_col),
+            *[
+                (
+                    pl.col(col).shift(1).log1p()
+                    .rolling_sum(window_size=window)
+                    .exp() - 1.0
+                )
+                .alias(f"{col}_mom{window}{units}") # just need to shift 1 month to apply lag
+                for col in self.asset_cols
+            ]
+        )
+        return result
         
     def mom1m(self) -> pl.DataFrame:
         """
@@ -41,7 +71,11 @@ class Features:
         result = self.data.select(
             pl.col(self.date_col),
             *[
-                pl.col(col).shift(1).alias(f"{col}_mom1m")
+                (
+                    pl.col(col).shift(1).log1p()
+                    .rolling_sum(window_size=)
+                )
+                .alias(f"{col}_mom1m") # just need to shift 1 month to apply lag
                 for col in self.asset_cols
             ]
         )
@@ -55,11 +89,13 @@ class Features:
             self.date_col,
             *[
                 (
-                    pl.col(col).shift(1).log1p()
-                    .rolling_sum(window_size=11)
+                    pl.col(col).shift(1).log1p() # sum logs then exponentiate to avoid under/overflow
+                    .rolling_sum(window_size=11) # includes current row and previous 11 rows = 12 rows
                     .exp() - 1.0
                 ).alias(f"{col}_mom12m")
                 for col in self.asset_cols 
             ]
         )
         return result
+
+    def vol3m(self)
