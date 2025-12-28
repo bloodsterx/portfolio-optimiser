fdb9dce5081e890469648330d8adea5598a3e991
bloodsterx <rickhonda0@gmail.com>
2025-12-28 15:35:27 +1100

    feat: add post request (learning front-end)

--- src/server/app.py
+++ src/server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from datetime import datetime
 
 app = Flask(__name__)
@@ -15,5 +15,18 @@ def get_data():
         "status": "success" # non-essential, but professional convention
     })
 
+@app.route("/greet", methods=["POST"])
+def greet():
+    data = request.get_json()
+
+    print(f"received data from browser: {data}")
+    
+    user_name = data.get("name", "").strip()
+    if not user_name:
+        user_name = "stranger"
+    
+    result = f"No {user_name}, I am your father"
+    return jsonify({"lebron": result})
+    
 if __name__ == "__main__":
     app.run(debug=True)
--- src/server/static/script.js
+++ src/server/static/script.js
@@ -7,3 +7,20 @@ document.getElementById("magicButton").addEventListener("click", () => {
         document.getElementById("responseArea").innerText = data.message
     });
 });
+
+document.getElementById("greetButton").addEventListener("click", () => {
+    const name = document.getElementById("userName").value;
+
+    fetch("/greet", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name: name }) // send to the flask backend
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("greetingResponse").innerText = data.lebron;
+        console.log("Received Username Data Successfully!");
+    });
+});
